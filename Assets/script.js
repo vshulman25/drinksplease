1f12cc3f7a74677a5601e52f01f0d2260041bea6
Matt Duffield <duffieldml@gmail.com>
2021-01-18 11:06:57 -0500

    Working on pseudo code for js

--- Assets/script.js
+++ Assets/script.js
@@ -4,10 +4,35 @@
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
 // <script src="Assets/script.js"></script>
 
+// We will need global variables defined if they will need to be called by multiple functions
 let drinkName;
 let liquorName;
+let userLocation;
+
+// On click events to capture when find drinks button is pressed
+
+//Ajax call for weather API
+
+    //Pull temp out out of returned array
+
+    //Check to see if what set of rules the temp falls in
+
+      //If outside temp < 50 degrees, recommend hot drinks
+      //This most likely will need to pull in a predefined array of drinks
+      //Only around 20 drinks are hot, would be easy to make a seperate array js page for that
+
+      //If outside temp > 50, recommend cold drinks
+
+      //Code to update DOM with the returned drinks
 
 
+// Code to save previously searched locations to the local storage
+
+// Code to save previously searched drinks/alcohol to the local storage
+
+// On click events to capture when the search button is pressed, or if user is not happy with the recommended drink
+
+// Ajax call for searching by drink name
 $("#name-search").on("click", function (event) {
     drinkName = $("#drink-name").val().trim();
     event.preventDefault();
@@ -23,8 +48,13 @@ $("#name-search").on("click", function (event) {
             console.log(response);
         });
 
+      //Returns an array of info
+
+      //Code for updating DOM with returned info
+
 });
 
+// Ajax call for searching by liquor type, might be able to throw this up into the ajax above
 $("#liquor-search").on("click", function (event) {
     liquorName = $("#liquor-name").val().trim();
     event.preventDefault();
@@ -40,4 +70,9 @@ $("#liquor-search").on("click", function (event) {
             console.log(response);
         });
 
+      //Returns an array of info
+
+      //Code for updating DOM with returned info
+
 });
+
